a7e962c5d22099f33f973f5b3badc03fed48fceb
Unknown <humberto.dias@techne.com.br>
2018-01-16 10:37:23 +0100

    CheckStyle

--- 0x01/arithmetic_asm.c
+++ 0x01/arithmetic_asm.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern int sum(int,int);
-extern int subtract(int,int);
-extern int multiply(int,int);
-extern int division(int,int);
-extern int divmod(int,int);
+extern int _sum(int, int);
+extern int _subtract(int, int);
+extern int _multiply(int, int);
+extern int _division(int, int);
+extern int _divmod(int, int);
 
 int main(int argc, char ** argv)
-{	
-   int n1, n2, ret;
-   char op;
-   while (scanf("%d %c %d", &n1, &op, &n2)==3){
-     switch(op){
-       case '+': ret = sum(n1,n2); break;
-       case '-': ret = subtract(n1,n2); break;
-       case 'x': ret = multiply(n1,n2); break;
-       case '/': ret = division(n1,n2); break;
-       case '%': ret = divmod(n1,n2); break;
-       default: {printf("Invalid operator [%c]! Should be +, -, x,  / or %%\n", op); exit(2); }
-     }
-     printf("%d %c %d = %d\n",n1,op,n2, ret);
+{
+  int n1, n2, ret;
+  char op;
+  while (scanf("%d %c %d", &n1, &op, &n2) == 3) {
+    switch (op) {
+    case '+': ret = _sum(n1, n2);      break;
+    case '-': ret = _subtract(n1, n2); break;
+    case 'x': ret = _multiply(n1, n2); break;
+    case '/': ret = _division(n1, n2); break;
+    case '%': ret = _divmod(n1, n2);   break;
+    default: {printf("Invalid operator [%c]! Should be +, -, x,  / or %%\n", op); exit(2); }
     }
-   return 0;
+    printf("%d %c %d = %d\n", n1, op, n2, ret);
+  }
+  return 0;
 }
--- 0x01/generator.c
+++ 0x01/generator.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 #include <math.h>
 
-const char ops[] = {'+','-','x','/','%'};
+const char ops[] = {'+', '-', 'x', '/', '%'};
 
 int random_int(int min, int max)
 {
-   return min + rand() % (max+1 - min);
+	return min + rand() % (max + 1 - min);
 }
 
-char get_random_operator(){
-	return ops[random_int(0,4)];
+char get_random_operator() {
+	return ops[random_int(0, 4)];
 }
 
-int main(int argc, char * argv[]){
-	if(argc < 2) { printf("Usage generator COUNT_LINES\n"); exit(1); }
+int main(int argc, char * argv[]) {
+	if (argc < 2) { printf("Usage generator COUNT_LINES\n"); exit(1); }
 	int max = atol(argv[1]);
-	for(int i=0; i < max;i++){
-		printf( "%d %c %d\n" , random_int(1, max), get_random_operator(), random_int(1,max) );
+	for (int i = 0; i < max; i++) {
+		printf( "%d %c %d\n" , random_int(1, max), get_random_operator(), random_int(1, max) );
 	}
 	return 0;
 }
